eca9f80dab69543a784494a418c55174fd7ec843
RaphaelRozman <133022096+RaphaelRozman@users.noreply.github.com>
2023-05-10 00:55:36 +0200

    finish 13

--- src/Blatt3/Aufgabe13/B.java
+++ src/Blatt3/Aufgabe13/B.java
@@ -10,11 +10,11 @@ public class B {
         for (int i = 0; i < zufallarr.length; i++) {
             zufallarr[i] = (char) ('A' + rd.nextInt(26));
         }
-
         Arrays.sort(zufallarr);
         if (Arrays.binarySearch(zufallarr, 'X') < 0) {
             System.out.println("X kommt nicht vor");
-            System.out.println("es sind " + Math.abs(Arrays.binarySearch(zufallarr, 'X')) + " Buchstaben kleiner als X");
-        }
+            System.out.println("es sind " + (Math.abs(Arrays.binarySearch(zufallarr, 'X'))-1) + " Buchstaben kleiner als X");
+        }else
+            System.out.println("X kommt an der " + (Arrays.binarySearch(zufallarr,'X')+1) + ".Stelle vor");
     }
 }
