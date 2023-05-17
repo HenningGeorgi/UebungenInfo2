5a2b140534ac626e97f8bff1cf09b3be5eb615f4
henni <henning.hg.georgi@t-online.de>
2023-05-17 18:56:59 +0200

    added new branch & Blatt4 Aufgabe 19

---
+++ src/Blatt4/Aufgabe19/A.java
@@ -0,0 +1,15 @@
+package Blatt4.Aufgabe19;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.function.UnaryOperator;
+
+public class A {
+    public static void main(String[] args) {
+        ArrayList<String> stringList = new ArrayList<String>(Arrays.asList(args));
+        UnaryOperator<String> upperCase = String::toUpperCase;
+        stringList.forEach(upperCase::apply);
+        stringList.removeIf(s -> (s.startsWith("A")));
+        stringList.forEach(System.out::println);
+    }
+}
---
+++ src/Blatt4/Aufgabe19/B.java
@@ -0,0 +1,6 @@
+package Blatt4.Aufgabe19;
+
+public class B {
+    public static void main(String[] args) {
+    }
+}
