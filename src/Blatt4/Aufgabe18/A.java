11b5f4189b4dcf2d3794c30017173d6c1f62098d
RaphaelRozman <133022096+RaphaelRozman@users.noreply.github.com>
2023-05-28 22:16:49 +0200

    1

--- src/Blatt4/Aufgabe18/A.java
+++ src/Blatt4/Aufgabe18/A.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 
 public class A {
     public static void main(String[] args) {
-<<<<<<< HEAD
         HashMap<String,Integer> map = new HashMap<>();
         for (int i = 0; i < args.length;i++){
             for (int j = 0;j < args[i].length();j++) {
@@ -16,19 +15,17 @@ public class A {
             }
         }
         System.out.println(map);
-=======
-        HashMap<Character, Integer> map = new HashMap<>();
+        HashMap<Character, Integer> map1 = new HashMap<>();
         for (String arg : args) {
             char[] charArray = arg.toCharArray();
             for (char c : charArray) {
-                if (map.get(c) == null) {
-                    map.put(c, 1);
+                if (map1.get(c) == null) {
+                    map1.put(c, 1);
                 } else {
-                    int i = map.get(c);
-                    map.put(c, ++i);
+                    int i = map1.get(c);
+                    map1.put(c, ++i);
                 }
             }
         }
->>>>>>> origin/master
     }
 }
