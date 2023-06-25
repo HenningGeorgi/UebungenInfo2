5d62df40bebb1c783d5abeed2c849ca4b2dee29f
henni <henning.hg.georgi@t-online.de>
2023-06-25 20:06:16 +0200

    class instead of datatype & finished Aufgabe38+39b

--- src/Blatt8/Aufgabe3839b/Match.java
+++ src/Blatt8/Aufgabe3839b/Match.java
@@ -1,13 +1,18 @@
 package Blatt8.Aufgabe3839b;
 
 public class Match {
-    private Team team1;
-    private Team team2;
+    private final Team team1;
+    private final Team team2;
     private Team winner;
 
     public Match(Team team1, Team team2) throws IllegalArgumentException {
         if (team1.equals(team2)) throw new IllegalArgumentException();
         this.team1 = team1;
         this.team2 = team2;
+    }
+
+    public void setWinner(Team team) {
+        if (!team.equals(team1)) throw new IllegalArgumentException("only team1 or team2 can be the winner!");
+        winner = team;
     }
 }
